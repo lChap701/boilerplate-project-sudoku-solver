5cbf0780cd5621cf92ebb9d3d9f2ccb1d8d551fd
lChap701 <chapmanlucas201@gmail.com>
2021-08-25 15:21:28 -0500

    Created the first test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,6 +2,21 @@ const chai = require("chai");
 const assert = chai.assert;
 
 const Solver = require("../controllers/sudoku-solver.js");
-let solver;
+let solver = new Solver();
+const puzzleObj = require("../controllers/puzzle-strings");
+let puzzles = puzzleObj.puzzlesAndSolutions;
 
-suite("UnitTests", () => {});
+suite("UnitTests", () => {
+  /* My Tests */
+  suite("Puzzle String Tests", () => {
+    test("1)  Valid Length Test", () => {
+      const puzzle = puzzles[Math.floor(Math.random() * puzzles.length)][0];
+      const MESSAGE = solver.validate(puzzle);
+      assert.equal(
+        MESSAGE,
+        "valid",
+        `'valid' should have been returned, not '${MESSAGE}'`
+      );
+    });
+  });
+});
