e8cf812f75ee193257ed37a6228bd9b7cbb9e320
lChap701 <chapmanlucas201@gmail.com>
2021-08-25 15:31:08 -0500

    Finished 'Validate Puzzle Strings Tests' suite

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -8,7 +8,7 @@ let puzzles = puzzleObj.puzzlesAndSolutions;
 
 suite("UnitTests", () => {
   /* My Tests */
-  suite("Puzzle String Tests", () => {
+  suite("Validate Puzzle String Tests", () => {
     test("1)  Valid Length Test", () => {
       const puzzle = puzzles[Math.floor(Math.random() * puzzles.length)][0];
       const MESSAGE = solver.validate(puzzle);
@@ -16,6 +16,26 @@ suite("UnitTests", () => {
         MESSAGE,
         "valid",
         `'valid' should have been returned, not '${MESSAGE}'`
+      );
+    });
+
+    test("2)  Invalid Characters Test", () => {
+      const puzzle = puzzles[Math.floor(Math.random() * puzzles.length)][1];
+      const MESSAGE = solver.validate(puzzle);
+      assert.equal(
+        MESSAGE,
+        "Invalid characters in puzzle",
+        `'Invalid characters in puzzle' should have been returned, not '${MESSAGE}'`
+      );
+    });
+
+    test("3)  Invalid Length Test", () => {
+      const puzzle = puzzles[Math.floor(Math.random() * puzzles.length)][1][0];
+      const MESSAGE = solver.validate(puzzle);
+      assert.equal(
+        MESSAGE,
+        "Expected puzzle to be 81 characters long",
+        `'Expected puzzle to be 81 characters long' should have been returned, not '${MESSAGE}'`
       );
     });
   });
