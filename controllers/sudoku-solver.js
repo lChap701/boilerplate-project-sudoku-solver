4ec552997265fe59e5504b122bf0f3c901136266
lChap701 <chapmanlucas201@gmail.com>
2021-08-25 10:04:53 -0500

    Added comments and formatted existing comments

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,52 @@
+/**
+ * Module for solvung Sudoku puzzles
+ * @module ./controllers/sudoku-solver
+ *
+ */
 class SudokuSolver {
+  /**
+   * Make sure the puzzle can be solved
+   * @param {String} puzzleString   Represents the entire puzzle
+   *
+   */
+  validate(puzzleString) {}
 
-  validate(puzzleString) {
-  }
+  /**
+   * Checks if the value entered is in the puzzle at the specified row and column
+   * @param {String} puzzleString   Represents the entire puzzle
+   * @param {String} row            Represents the specified row (A - I)
+   * @param {Number} column         Represents the specified column (1 - 9)
+   * @param {Number} value          Represents the value to look for
+   *
+   */
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  /**
+   * Checks if the value entered is in the puzzle at the specified row and column
+   * @param {String} puzzleString   Represents the entire puzzle
+   * @param {String} row            Represents the specified row (A - I)
+   * @param {Number} column         Represents the specified column (1 - 9)
+   * @param {Number} value          Represents the value to look for
+   *
+   */
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  }
+  /**
+   * Checks if the value entered is in the puzzle at the specified row and column
+   * @param {String} puzzleString   Represents the entire puzzle
+   * @param {String} row            Represents the specified row (A - I)
+   * @param {Number} column         Represents the specified column (1 - 9)
+   * @param {Number} value          Represents the value to look for
+   *
+   */
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
-
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  /**
+   * Solves the puzzle
+   * @param {String} puzzleString   Represents the entire puzzle
+   *
+   */
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,16 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
+/**
+ * Module that handles most of the routing
+ * @param {*} app   Represents the entire application
+ *
+ */
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {});
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-
-    });
+  app.route("/api/solve").post((req, res) => {});
 };
