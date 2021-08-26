559c407b20ceadfa357d574ddeeca99c0f08ee06
lChap701 <chapmanlucas201@gmail.com>
2021-08-25 23:43:33 -0500

    Finished routing fo /api/check

--- routes/api.js
+++ routes/api.js
@@ -12,10 +12,49 @@ module.exports = function (app) {
 
   app.route("/api/check").post((req, res) => {
     console.log(req.body);
-    //req.body.puzzle
-    //req.body.coordinate.split("")[0]
-    //req.body.coordinate.split("")[1]
-    //req.body.value
+    let conflicts = [];
+
+    // Gets the data that was submitted
+    let puzzle = req.body.puzzle;
+    let row = req.body.coordinate.split("")[0];
+    let col = req.body.coordinate.split("")[1];
+    let value = req.body.value;
+
+    // Validates the data and stores the result
+    let rowChkRes = solver.checkRowPlacement(puzzle, row, parseInt(col), value);
+    let colChkRes = solver.checkColPlacement(puzzle, row, parseInt(col), value);
+    let regChkRes = solver.checkRegionPlacement(
+      puzzle,
+      row,
+      parseInt(col),
+      value
+    );
+
+    // Checks the results of the row validation
+    if (typeof rowChkRes === String) {
+      return { error: rowRes };
+    } else if (!rowChkRes) {
+      conflicts.push("row");
+    }
+
+    // Checks the results of the column validation
+    if (typeof colChkRes === String) {
+      return { error: colChkRes };
+    } else if (!colChkRes) {
+      conflicts.push("column");
+    }
+
+    // Checks the results of the region validation
+    if (typeof regChkRes === String) {
+      return { error: regChkRes };
+    } else if (!regChkRes) {
+      conflicts.push("region");
+    }
+
+    // Checks if any conflicts were found during validation
+    if (conflicts.length > 0) return { valid: false, conflict: conflicts };
+
+    return { valid: true };
   });
 
   app.route("/api/solve").post((req, res) => {
