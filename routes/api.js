73a0df7370e8cf4adc3e718f8e8dc54c9ae5a53f
lChap701 <chapmanlucas201@gmail.com>
2021-08-25 19:38:57 -0500

    Started routing for /api/check

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,13 @@ const SudokuSolver = require("../controllers/sudoku-solver.js");
 module.exports = function (app) {
   let solver = new SudokuSolver();
 
-  app.route("/api/check").post((req, res) => {});
+  app.route("/api/check").post((req, res) => {
+    console.log(req.body);
+    //req.body.puzzle
+    //req.body.coordinate.split("")[0]
+    //req.body.coordinate.split("")[1]
+    //req.body.value
+  });
 
   app.route("/api/solve").post((req, res) => {
     console.log(req.body);
